9477ee7e3fd939a08cb1a4e00cd716ab62449985
cimsapp <55845968+sheikhshakil@users.noreply.github.com>
2022-03-03 19:58:03 +0600

    Add project files.

---
+++ OutputCachingCore/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OutputCachingCore.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace OutputCachingCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        [OutputCache(Duration = 20, VaryByParam = "user")]
+        public IActionResult Index(string user = "")
+        {
+
+            DateTime date = DateTime.Now;
+
+            return View(date);
+        }
+    }
+}
